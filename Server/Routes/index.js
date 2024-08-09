983dd37f8df8fa8f8524b20987e745badb689024
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-08-09 18:17:36 +0300

    feat: add course route

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const taskRoutes = require('./taskRoute');
+const courseRoutes = require('./courseRoutes');
 
 router.use("/auth",authRoutes);
 router.use("/user",userRoutes);
 router.use("/tasks", taskRoutes);
+router.use("/courses", courseRoutes);
 
 module.exports = router;
