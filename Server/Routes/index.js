3c35377651fe8a5726c36658096daa8866cc9453
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-08-09 23:33:58 +0300

    update inexd route include content

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -4,10 +4,12 @@ const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const taskRoutes = require('./taskRoute');
 const courseRoutes = require('./courseRoutes');
+const contentRoutes = require('./contentRoutes');
 
 router.use("/auth",authRoutes);
 router.use("/user",userRoutes);
 router.use("/tasks", taskRoutes);
 router.use("/courses", courseRoutes);
+router.use("/content", contentRoutes);
 
 module.exports = router;
