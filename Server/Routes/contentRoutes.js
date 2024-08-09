4e2ab29f4a0dc3edf31d6f53f67e071e3bb32b09
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-08-09 23:47:09 +0300

    feat: add update and delete content routes

--- Server/Routes/contentRoutes.js
+++ Server/Routes/contentRoutes.js
@@ -1,9 +1,11 @@
-const {createContent} = require('../Controllers/contentController');
+const {createContent, updateContent, deleteContent } = require('../Controllers/contentController');
 const verifyRole = require('../utils/verifyRole');
 const uploadMiddleware = require('../middlewares/uploadMiddleware');
 const {contentValidator} = require('../utils/validators/contentValidator');
 const router = require('express').Router();
 
 router.post("/upload", verifyRole("educator"), uploadMiddleware.single('contentFile'),contentValidator, createContent);
+router.put("/:id", verifyRole("educator"), contentValidator, updateContent);
+router.delete("/:id", verifyRole("educator"), deleteContent);
 
 module.exports = router;
