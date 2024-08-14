6425694c354cf566f2712380371a3bf688e845f7
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-08-14 16:14:33 +0300

    Fix content validator

--- Server/utils/validators/contentValidator.js
+++ Server/utils/validators/contentValidator.js
@@ -6,17 +6,20 @@ exports.contentValidator = [
     check('description')
         .notEmpty().withMessage('Description is required'),
     check('type')
-        .notEmpty().withMessage('Type is required'),
-    check('url')
-        .notEmpty().withMessage('URL is required'),
-    check('file')
-        .notEmpty().withMessage('File is required'),
-    check('text')
-        .notEmpty().withMessage('Text is required'),
+        .notEmpty().withMessage('Content Type is required')
+        .isIn(['link', 'file', 'text']).withMessage('Type must be one of "link", "file", or "text"'),
     check('lectureId')
         .notEmpty().withMessage('Lecture ID is required')
         .isInt().withMessage('Lecture ID must be an integer'),
     check('uploadedBy')
         .notEmpty().withMessage('Uploaded By is required')
         .isInt().withMessage('Uploaded By must be an integer'),
+
+    body().custom((value, { req }) => {
+        const { url, file, text } = req.body;
+        if ([url, file, text].filter(Boolean).length !== 1) {
+            throw new Error('Only one of "url", "file", or "text" must be provided');
+        }
+        return true;
+    })
 ];
