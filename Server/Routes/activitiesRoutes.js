6e089d65200e076b0ba433fb1834d10b7eedc8a0
Your Name <you@example.com>
2024-08-15 17:38:36 +0300

    change the role access to admin

--- Server/Routes/activitiesRoutes.js
+++ Server/Routes/activitiesRoutes.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 
 // I don't think this resourse can be public so i limited it for admin.
 
-router.route('/').get(verifyRole('educator'), getActivities);
-router.route('/:activityId').get(verifyRole('educator'), getActivity);
+router.route('/').get(verifyRole('admin'), getActivities);
+router.route('/:activityId').get(verifyRole('admin'), getActivity);
 
 module.exports = router;
